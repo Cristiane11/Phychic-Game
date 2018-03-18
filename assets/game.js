c27deed404e8f6b11cb5986b333eee6d84aa8366
Cristiane Abreu <bjwmcd@yahoo.com>
2018-03-18 17:29:27 -0400

    phsychic game files

--- assets/game.js
+++ assets/game.js
@@ -1,8 +1,9 @@
-var guessChoice = ("")
+var guessChoice = event.key;
 var computerChoice = ["a", "b", "...", "z"]
 
 document.write("<h1 id=titleAnimation> Phsychic Game </h1>")
 document.getElementById("titleAnimation").innerHTML = "My Phsychic Game";
+
 var guessChoice = prompt("Guess What Letter I'm thinking of ?");
 
 
@@ -11,6 +12,7 @@ document.onkeyup = function(event) {
 
 
         var guessChoice = String.fromCharCode(event.keycode).toLowerCase();
+        var computerChoice = computerChoice[Math.floor(Math.random() * computerChoice.length)];
 
 
         if (guessChoice === computerChoice) {
